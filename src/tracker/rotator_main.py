9b0741366bce1d6c09621641566fd37614d0d2b2
AntonyKT <antonykartomithomas@gmail.com>
2022-02-12 23:17:17 +1100

    comment

--- src/tracker/rotator_main.py
+++ src/tracker/rotator_main.py
@@ -10,7 +10,7 @@ el_motor = Stepper()
 def interpolate_pos_data(orig_arr):
 	return interpolate.interp1d(orig_arr)
 
-def main():
+def main(): #put in tracking data download logic when topocentric data is bug fixed.
 	positions = [...] #2d array [3 columns] (t, az, el) of positions during pass
 	az_func = interpolate.interp1d(positions[:, [0,1]], kind='cubic')
 	el_func = interpolate.interp1d(positions[:, [0,2]], kind='cubic')
